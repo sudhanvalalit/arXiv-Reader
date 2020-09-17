17d74ee21b6a2de2bf820c754ee7d04d4f2df8b4
Sudhanva Lalit <sudhanvalalit@github.com>
2020-09-17 14:29:16 -0400

    Added basic app and Readme file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # arXiv-Reader
+
+This is an arXiv Reader app. Using this one can search papers, read them online and download them and save them. 
---
+++ arXivReader/android/app/src/main/kotlin/com/arXivReader/arXivReader/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arXivReader.arXivReader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
